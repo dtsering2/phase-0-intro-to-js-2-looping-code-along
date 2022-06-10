dd634a77f65c6d3db2b6a23bb6a577c07fd870d5
Tsering Tsering <dtsering2@gmail.com>
2022-06-09 22:32:03 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function writeCards(names,event){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    
+    return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown (number){
+    while (number >  -1){
+        console.log(number--);
+    }
+    return number;
+}
+
+countDown(10);
+
+
+
+
+
